b19dc94da8d2980c13f665466f89930189ac9035
TAKEDA Takashi <takeda.takashi@classmethod.jp>
2019-09-20 10:29:33 +0900

    corrected indent

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ const app = express();
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    res.json({ message: 'Hello World!' });
+  res.json({ message: 'Hello World!' });
 });
 
 router.get('/bye', (_req, res) => {
-    res.json({ message: 'Bye!' });
+  res.json({ message: 'Bye!' });
 });
 
 app.use('/', router);
