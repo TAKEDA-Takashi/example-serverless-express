b1ec2f91f6986d78062230e0233cc900d2517726
TAKEDA Takashi <takeda.takashi@classmethod.jp>
2019-09-20 10:12:10 +0900

    impl simple rest api

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as awsServerlessExpress from 'aws-serverless-express';
+import 'source-map-support/register';
+
+import app from './src/app';
+
+const proxy = awsServerlessExpress.createServer(app);
+
+export const server = (event, context) => {
+  awsServerlessExpress.proxy(proxy, event, context);
+}
+
+if (require.main === module) {
+  // called directly i.e. "node app"
+  app.listen(3000, (err) => {
+    if (err) console.error(err);
+    console.log('server listening on 3000');
+  });
+}
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+    res.json({ message: 'Hello World!' });
+});
+
+router.get('/bye', (_req, res) => {
+    res.json({ message: 'Bye!' });
+});
+
+app.use('/', router);
+
+export default app;
